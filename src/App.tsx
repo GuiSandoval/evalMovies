508c4347aed65632dd5a7d680d9ec7e68e2d33fc
Guilherme <guijpjs@gmail.com>
2021-12-04 03:51:41 -0300

    chore ✨: Create GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Olá Mundo</h1>
     </div>
   );
