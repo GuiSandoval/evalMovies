14a255f065e8754e5311edeb276699c7a12004c7
Guilherme <guijpjs@gmail.com>
2021-12-07 21:04:05 -0300

    chore ✨ : Create Form to Add Film

--- src/components/ModalNewFilm/index.tsx
+++ src/components/ModalNewFilm/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal'
+import { Container } from './styles'
 
 interface IProps {
     isOpen: boolean,
@@ -9,7 +10,21 @@ interface IProps {
 export const ModalNewFilm = ({ isOpen, isHandleClose }: IProps) => {
     return (
         <Modal isOpen={isOpen} onRequestClose={isHandleClose}>
-            <h2> Avalie um Filme</h2>
+            <Container>
+                <h2> Avalie um Filme</h2>
+
+                <input type="text" placeholder="Titulo do filme" />
+                <input type="text" placeholder="Descriçao do filme" />
+                <input type="text" placeholder="Autor" />
+                <input type="text" placeholder="Ano do filme" />
+                <input type="text" placeholder="Nota do filme" />
+                <input type="text" placeholder="Avaliação do filme" />
+
+
+
+                <button type="submit">Enviar</button>
+                
+            </Container>
         </Modal>
     )
 }
