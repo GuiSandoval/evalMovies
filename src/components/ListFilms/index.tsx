4032276333f87b07ed2f239a92fbd7ef8e1dbb8c
Guilherme <guijpjs@gmail.com>
2021-12-09 00:14:19 -0300

    chore ✨ : Add Input Search

--- src/components/ListFilms/index.tsx
+++ src/components/ListFilms/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
 import { apiOMDB } from "../../services/api"
 import { FilmPreview } from "../FilmPreview"
-import { Container } from "./styles"
+import { AreaFilms, Container } from "./styles"
 
 interface IFilms {
     Poster: string;
@@ -15,16 +16,29 @@ export const ListFilms = () => {
 
     const [isLoading, setIsLoading] = useState(true)
     const [films, setFilms] = useState<IFilms[]>([])
+    const [searchText, setSearchText] = useState("")
 
     function getFilms() {
         apiOMDB.get("", {
             params: {
-                s: 'star wars',
+                s: searchText,
             }
         }).then(response => {
             setFilms(response.data.Search)
             setIsLoading(false)
         })
+    }
+
+    function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        const text = event.target.value;
+        setSearchText(text)
+    }
+
+    function handleEnterKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === 'Enter') {
+
+            getFilms();
+        }
     }
 
     useEffect(() => {
@@ -34,18 +48,26 @@ export const ListFilms = () => {
 
     return (
         <Container>
-            {!isLoading && films.map(film => {
-                return (
-                    <FilmPreview
-                        key={film.imdbID}
-                        poster={film.Poster}
-                        title={film.Title}
-                        type={film.Type}
-                        year={film.Year}
-                        imdbID={film.imdbID} />
+            <input
+                type="text"
+                onChange={handleSearch}
+                onKeyPress={handleEnterKeyPress}
+                placeholder="Procure um filme" />
+            <AreaFilms>
 
-                )
-            })}
+                {!isLoading && films?.map(film => {
+                    return (
+                        <FilmPreview
+                            key={`${film.imdbID}-${film.Title}`}
+                            poster={film.Poster}
+                            title={film.Title}
+                            type={film.Type}
+                            year={film.Year}
+                            imdbID={film.imdbID} />
+
+                    )
+                })}
+            </AreaFilms>
         </Container>
     )
 }
--- src/components/ListFilms/styles.ts
+++ src/components/ListFilms/styles.ts
@@ -2,10 +2,25 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
+    /* flex-wrap: wrap; */
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width:100%;
+    padding: 4rem 0 0;
+
+    input {
+        width: 100%;
+        margin: 0 0 1rem 0;
+        padding: 1rem;
+    }
+`;
+export const AreaFilms = styled.div`
+    display: flex;
     flex-wrap: wrap;
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
     width:100%;
-    padding: 4rem 0 0;
+    /* padding: 4rem 0 0; */
 `;
