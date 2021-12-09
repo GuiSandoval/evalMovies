f2cb1346cbaafaa8b597134c5c369d8d867f1d16
Guilherme <guijpjs@gmail.com>
2021-12-08 23:12:22 -0300

    style ✏️ : List Films

--- src/components/ListFilmsAPI/styles.ts
+++ src/components/ListFilmsAPI/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: flex-start;
+flex-wrap: wrap;
+`;
