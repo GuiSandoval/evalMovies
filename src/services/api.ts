b963951bf92f8aba7b2700b0b171581fef4c0f48
Guilherme <guijpjs@gmail.com>
2021-12-08 23:03:27 -0300

    Fix 🐞 : Using single API

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const apiOMDB = axios.create({
     baseURL: 'http://www.omdbapi.com/?apikey=719787b6',
 })
+
+export const api = axios.create({
+    baseURL: 'http://localhost:3333',
+});
