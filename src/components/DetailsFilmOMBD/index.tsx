3b3fc32594b99f9869206c9754e0e3cfb4d675d4
Guilherme <guijpjs@gmail.com>
2021-12-09 00:44:40 -0300

    style ✏️ : Page Avaliar

--- src/components/DetailsFilmOMBD/index.tsx
+++ src/components/DetailsFilmOMBD/index.tsx
@@ -1,15 +1,53 @@
-import React, { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Container } from './styles'
+import { apiOMDB } from '../../services/api'
+import { Container, InfoFilm } from './styles'
 
 export function DetailsFilmOMBD() {
-    const {idFilm: idFilmParam}= useParams()
-    const [idFilm, setIdFilm] = useState(idFilmParam)
-    
+    const { idFilm: idFilmParam } = useParams()
+    const [idFilm] = useState(idFilmParam)
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [film, setFilm] = useState<any>({})
+
+    function getFilm() {
+        apiOMDB.get("", {
+            params: {
+                i: idFilm,
+                plot: "full",
+            }
+        }).then(response => {
+            setFilm(response.data)
+            setIsLoading(false)
+        })
+    }
+
+    useEffect(() => {
+        getFilm()
+    }, [])
+
+
     return (
         <Container>
-            <h1>Olá Mundo</h1>
-            {idFilm}
+            {isLoading ? (<h1>Carregando...</h1>) : (
+                <>
+                    <InfoFilm>
+                        <img src={film?.Poster} alt="" />
+                        <div className="wrapper-film">
+
+                            <h1 className="title">{film?.Title}</h1>
+                            <div className="info-film">
+                                <p>{film?.Year}</p>
+                                <p>{film?.Writer}</p>
+                                <p>{film?.Runtime}</p>
+                            </div>
+                            <p className="description-film">{film?.Plot}</p>
+                        </div>
+                    </InfoFilm>
+                    
+                </>
+            )}
         </Container>
     )
 }
--- src/components/DetailsFilmOMBD/styles.ts
+++ src/components/DetailsFilmOMBD/styles.ts
@@ -2,3 +2,38 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 `;
+
+export const InfoFilm = styled.div`
+display: flex;
+flex-direction: revert;
+justify-content: space-between;
+align-items: flex-start;
+padding: 48px 0 0 0;
+.wrapper-film{
+    padding: 0 1rem;
+    .title {
+        /* font-size: 1.5rem; */
+        color: #fff;
+    }
+    
+    .info-film{
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        color: #a1a1a1;
+        
+        > *{
+            margin-right: 1rem;
+        }
+        
+    }
+    
+    .description-film {
+        color: #fff;
+        margin: 10px 0;
+        font-size: 1rem;
+        line-height: 1.6em;
+    }
+}
+    `;
